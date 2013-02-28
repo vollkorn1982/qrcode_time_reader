fece43009932134e0803fe6e160c008af1f73e61
Jan Girlich <vollkorn@cryptobitch.de>
2013-02-28 22:51:13 +0100

    first working version. I shall call it a beta!

--- qrcode_time_reader.cpp
+++ qrcode_time_reader.cpp
@@ -15,6 +15,7 @@
 #include <QDateTime>
 #include <QFile>
 #include <QDir>
+#include <QPushButton>
 
 #include "resizinglabel.h"
 
@@ -36,10 +37,15 @@ qrcode_time_reader::qrcode_time_reader(QWidget *parent) :
   m_preview = findChild<ResizingLabel*>("photoPreview");
   m_exifLabel = findChild<QLabel*>("exifTime_data");
   m_qrLabel = findChild<QLabel*>("qrTime_data");
-  m_timeDifference = findChild<QLabel*>("timeDiff_data");
+  m_timeDifferenceLabel = findChild<QLabel*>("timeDiff_data");
 
   m_currentPicQRDateTime.setTimeSpec(Qt::UTC);
   m_currentPicExifDateTime.setTimeSpec(Qt::UTC);
+
+  m_timeDifference = 0; //!< 0 means no time difference->fix time button disabled
+
+  m_fixTimeButton = findChild<QPushButton*>("fixTimes");
+  connect(m_fixTimeButton, SIGNAL(clicked()), this, SLOT(adjustAllTimes()));
 }
 
 qrcode_time_reader::~qrcode_time_reader()
@@ -50,35 +56,50 @@ qrcode_time_reader::~qrcode_time_reader()
 //! \brief Opens a file dialog for the user to select files to load.
 void qrcode_time_reader::openFiles()
 {
-  m_files = QFileDialog::getOpenFileNames(
+  QStringList filenames = QFileDialog::getOpenFileNames(
         NULL, NULL,
         "",
         tr("Photos (*.jpg *.jpeg)"),
         NULL,
         NULL);
 
-  m_photoTable->setRowCount(m_files.count());
-  for (int i = 0; i < m_files.count(); ++i) {
-      QString filename = m_files[i];
-      QTableWidgetItem *name = new QTableWidgetItem(filename.remove(0, m_files[i].lastIndexOf("/") + 1));
+  m_photoTable->setRowCount(filenames.count());
+  for (int i = 0; i < filenames.count(); ++i) {
+      PictureInfo picInfo;
+      picInfo.filename = filenames[i];
+
+      QTableWidgetItem *name = new QTableWidgetItem(filenames[i].remove(0, filenames[i].lastIndexOf("/") + 1));
       name->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
       m_photoTable->setItem(i, 0, name);
 
+      bool bOk = false;
+      picInfo.exifTime = readExifTime(picInfo.filename, bOk);
+      picInfo.qrTime = readQRCodeTime(picInfo.filename, bOk);
+
       QTableWidgetItem *status = new QTableWidgetItem(tr("<reading>"));
       status->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+
+      if (bOk)
+        {
+          QDateTime time;
+          time.setTime_t(picInfo.qrTime);
+          status->setText(time.toString());
+        }
+
       m_photoTable->setItem(i, 1, status);
+      m_pictures.append(picInfo);
     }
 }
 
 //! \brief Displays the times and their difference in the preview
-//! @param[in] currentRow the row corresponds to the index in
+//! @param[in] currentRow the row corresponds to the index in \a m_files
 void qrcode_time_reader::previewCurrentPhoto(int currentRow, int /* currentColumn */, int /* previousRow */, int /* previousColumn */)
 {
-  m_preview->setPixmap(QPixmap(m_files[currentRow]));
-  qDebug() << QImage(m_files[currentRow]).text();
+  m_preview->setPixmap(QPixmap(m_pictures[currentRow].filename));
+  qDebug() << QImage(m_pictures[currentRow].filename).text();
 
   bool exifOk = false;
-  uint exifTime = displayExif(m_files[currentRow], exifOk);
+  uint exifTime = readExifTime(m_pictures[currentRow].filename, exifOk);
   if (exifOk) {
       m_currentPicExifDateTime.setTime_t(exifTime);
       m_exifLabel->setText(m_currentPicExifDateTime.toString() + " UTC");
@@ -87,7 +108,7 @@ void qrcode_time_reader::previewCurrentPhoto(int currentRow, int /* currentColum
     m_exifLabel->setText(tr("<no data>"));
 
   bool qrOk = false;
-  uint qrTime = readQRCode(m_files[currentRow], qrOk);
+  uint qrTime = readQRCodeTime(m_pictures[currentRow].filename, qrOk);
   if (qrOk) {
       m_currentPicQRDateTime.setTime_t(qrTime);
       m_qrLabel->setText(m_currentPicQRDateTime.toString() + " UTC");
@@ -96,18 +117,22 @@ void qrcode_time_reader::previewCurrentPhoto(int currentRow, int /* currentColum
     m_qrLabel->setText(tr("<no data>"));
 
   if (qrOk && exifOk) {
-      int difference = exifTime - qrTime;
-      m_timeDifference->setText(QString::number(difference) + tr(" seconds"));
+      m_timeDifference = exifTime - qrTime;
+      m_fixTimeButton->setEnabled(m_timeDifference != 0);
+      m_timeDifferenceLabel->setText(QString::number(m_timeDifference) + tr(" seconds"));
     }
   else
-    m_timeDifference->setText(tr("<no data>"));
+    {
+      m_timeDifferenceLabel->setText(tr("<no data>"));
+      m_fixTimeButton->setEnabled(false);
+    }
 }
 
 //! \brief Read EXIF time header.
 //! @param[in] filename Path and filename of the picture to display
 //! @param[out] bOk false if no time could be read
-//! @return time in seconds since 01.01.1970, undefined if no time was found
-uint qrcode_time_reader::displayExif(const QString &filename, bool &bOk)
+//! @return time in seconds since 01.01.1970, 0 if no time was found
+uint qrcode_time_reader::readExifTime(const QString &filename, bool &bOk)
 {
   QFile file(filename);
   if (!file.open(QIODevice::ReadOnly)) {
@@ -137,7 +162,7 @@ uint qrcode_time_reader::displayExif(const QString &filename, bool &bOk)
 //! @param[in] filename Path and filename of the picture to display
 //! @param[out] bOk false if no time could be read
 //! @return time in seconds since 01.01.1970, undefined if no time was found
-uint qrcode_time_reader::readQRCode(const QString &filename, bool &bOk)
+uint qrcode_time_reader::readQRCodeTime(const QString &filename, bool &bOk)
 {
   QImage resizeImage(filename);
   resizeImage = resizeImage.scaledToWidth(640, Qt::SmoothTransformation);
@@ -175,4 +200,36 @@ uint qrcode_time_reader::readQRCode(const QString &filename, bool &bOk)
   qr_decoder_close(decoder);
   cvReleaseImage(&src);
   return time;
+}
+
+//! \brief Applies \a m_timeDifference to the EXIF time of all images in \a m_pictures
+//! and writes the new value to the files.
+void qrcode_time_reader::adjustAllTimes()
+{
+  for (int i = 0; i < m_pictures.size(); ++i) {
+      m_pictures[i].exifTime -= m_timeDifference;
+      Exiv2::Value::AutoPtr timeStr = Exiv2::Value::create(Exiv2::asciiString);
+
+      // Set the value to a string
+      QDateTime dateTime;
+      dateTime.setTime_t(m_pictures[i].exifTime);
+      timeStr->read(dateTime.toString("yyyy:MM:dd hh:mm:ss").toStdString().c_str());
+
+      // Add the value together with its key to the Exif data container
+      Exiv2::ExifKey key("Exif.Photo.DateTimeOriginal");
+      Exiv2::ExifData exifData;
+      exifData.add(key, timeStr.get());
+
+      Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(QFile::encodeName(m_pictures[i].filename).data());
+      Q_ASSERT(image.get() != 0);
+
+      image->setExifData(exifData);
+      image->writeMetadata();
+    }
+
+  m_timeDifference = 0;
+  m_fixTimeButton->setEnabled(false);
+
+  // reload the preview to reflect the changes
+  previewCurrentPhoto(m_photoTable->currentIndex().row(), 0, 0, 0);
 }
--- qrcode_time_reader.h
+++ qrcode_time_reader.h
@@ -7,6 +7,7 @@
 class QTableWidget;
 class ResizingLabel;
 class QLabel;
+class QPushButton;
 
 namespace Ui {
   class qrcode_time_reader;
@@ -23,21 +24,31 @@ public:
 protected slots:
   void openFiles();
   void previewCurrentPhoto(int currentRow, int currentColumn, int previousRow, int previousColumn);
+  void adjustAllTimes();
   
 private:
-  uint displayExif(const QString &filename, bool &bOk);
-  uint readQRCode(const QString &filename, bool &bOk);
+  struct PictureInfo {
+    QString filename;
+    uint exifTime;
+    uint qrTime;
+  };
+
+private:
+  uint readExifTime(const QString &filename, bool &bOk);
+  uint readQRCodeTime(const QString &filename, bool &bOk);
 
 private:
   Ui::qrcode_time_reader *ui;
-  QStringList             m_files; //!< List of all read filenames
+  QList<PictureInfo>      m_pictures; //!< List of all read filenames
+  int                     m_timeDifference;
   QTableWidget           *m_photoTable;
   ResizingLabel          *m_preview;
   QLabel                 *m_exifLabel;
   QLabel                 *m_qrLabel;
-  QLabel                 *m_timeDifference;
+  QLabel                 *m_timeDifferenceLabel;
   QDateTime               m_currentPicExifDateTime;
   QDateTime               m_currentPicQRDateTime;
+  QPushButton            *m_fixTimeButton;
 };
 
 #endif // QRCODE_TIME_READER_H
